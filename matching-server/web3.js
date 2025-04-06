5fd3658537f12395e41679bac18468b1e4228f6d
dkekzhs <dkekzhs5@naver.com>
2025-04-06 23:50:25 +0900

    chore abi file

--- matching-server/web3.js
+++ matching-server/web3.js
@@ -4,8 +4,11 @@ const contractJson = require('./abi/Matcher.json');
 
 const web3 = new Web3('http://localhost:8545'); // Besu RPC
 
-const contractAddress = '0xYourDeployedContractAddress';
-const account = '0xYourBesuAccount'; // Unlock된 계정
+const pk = "0x95afc758fbe4b2e21fd4938611b746205999f0588b28eb279efa804db4a598e7";
+const account = web3.eth.accounts.privateKeyToAccount(pk);
+const contractAddress = "0x115fc5B81318EA57395296C5a7b7F965e8b7615D";
+
+console.log(account);
 
 const matcherContract = new web3.eth.Contract(contractJson.abi, contractAddress);
 
